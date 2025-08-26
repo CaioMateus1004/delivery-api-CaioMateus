f3059bb06d6361dec0c772922133c4239add0981
Caio Mateus <srspine16@gmail.com>
2025-08-26 17:56:29 -0300

    Adiciona anotações Swagger para documentação de API nos controllers e DTOs de Produto e Restaurante

--- src/main/java/com/deliverytech/delivery/dto/response/RestauranteResponse.java
+++ src/main/java/com/deliverytech/delivery/dto/response/RestauranteResponse.java
@@ -4,15 +4,30 @@ import lombok.*;
 
 import java.math.BigDecimal;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RestauranteResponse {
+    @Schema(description = "Identificador unico do restaurante", example = "1")
     private Long id;
+
+    @Schema(description = "Nome do restaurante", example = "Restaurante do zé e cia")
     private String nome;
+
+    @Schema(description = "Categoria do restaurante", example = "pizzaria")
     private String categoria;
+
+    @Schema(description = "Telefone do restaurante", example = "99 6666-6666")
     private String telefone;
+
+    @Schema(description = "Taxa de entrega em Reais (R$)", example = "5.00")
     private BigDecimal taxaEntrega;
+
+    @Schema(description = "Tempo estimado de entrega em minutos", example = "30")
     private Integer tempoEntregaMinutos;
+
+    @Schema(description = "Disponibilidade do restaurante", example = "true")
     private Boolean ativo;
 }
